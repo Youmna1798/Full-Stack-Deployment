9287c96688c33f97fe21a3169ce2b275e59e3612
Youmna1798 <yoma.samy98@gmail.com>
2022-08-20 19:34:37 +0200

    edit

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   appName: "Udagram",
   // apiHost: 'http://localhost:3000/api/v0'
 
-  apiHost: "http://udagram-api-dev.eba-qva9wtmz.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api-dev1.eba-qva9wtmz.us-east-1.elasticbeanstalk.com//api/v0",
 };
 
 /*
